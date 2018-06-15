f5c72583cdd056137a8880e4cd874a50d4eccfb3
Matheus Toscano <ssstoscano@gmail.com>
2018-06-15 20:56:58 -0300

    IV
    Config menu in our prompt app

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const {ChatManager, TokenProvider} = require('@pusher/chatkit');
 const {JSDOM} = require('jsdom');
 const util = require('util');
+const prompt = require('prompt')
 
 const makeChatkitNodeCompatible = () => {
   const {window} = new JSDOM();
@@ -12,7 +13,18 @@ makeChatkitNodeCompatible();
 
 const main = async () => {
   try {
-    // TODO: We're going to create a command line chat client in here!
+    prompt.start();
+    prompt.message  = '';
+    const get = util.promisify(prompt.get);
+    const usernameSchema = [
+        {
+            description: 'Enter your username',
+            name: 'username',
+            required: true,
+        };
+    ];
+
+    const {username} = await get(usernameSchema);
   } catch (err) {
     console.log(`Failed with ${err}`);
     process.exit(1);
